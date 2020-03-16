82e41165b7ce0e7af30ed262766b54a602ea19c0
HanlonsStraightRazor <muellermartin98@gmail.com>
2020-03-16 01:00:17 -0500

    Removed bashrc append

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
-echo "# Helps with the lightline vim plugin" >> ~/.bashrc
-echo "export TERM=xterm-256color" >>  ~/.bashrc
 vim +PluginInstall +qall
 rm -- "$0"
