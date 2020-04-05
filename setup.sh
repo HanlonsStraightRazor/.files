65f1d28b68aeffab6de0590e411dd219bb325081
HanlonsStraightRazor <muellermartin98@gmail.com>
2020-04-05 15:28:54 -0500

    Restored setup script to its original version

--- setup.sh
+++ setup.sh
@@ -8,8 +8,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# Remove this file from repo
-git rm -- "$0"
-
 # Remove this file from working tree
 rm -- "$0"
