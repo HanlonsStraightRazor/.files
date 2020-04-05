84542848757c9f38c35c198d65caec709acbe1c2
HanlonsStraightRazor <muellermartin98@gmail.com>
2020-04-05 15:15:34 -0500

    Removed .gitignore and made it generate on startup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+################################################################################
 # Sets up this repo
+################################################################################
 
 # Install Vundle and all plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Add .gitignore and add this and bundle to it
+echo bundle > .gitignore
+echo setup.sh >> .gitignore
 
 # Remove this file
 rm -- "$0"
