2eae6998841866106dd1dff88d597b44a84ca812
HanlonsStraightRazor <muellermartin98@gmail.com>
2022-08-05 11:19:46 -0500

    Clean up Vim update script.
    - Reformatted script for better style
    - Install pywal via pip if pip is installed and pywal isn't
    - Removed "git pull" (now N/A because of merge)

--- .vim/update.sh
+++ .vim/update.sh
@@ -1,17 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 ################################################################################
-# Sets up or updates this repo.
+# Set up this directory or update plugins.
 ################################################################################
 
-# Install vim-plug and all plugins (if needed)
-if [ ! -f 'autoload/plug.vim' ]; then
+# Check if pip is installed
+if [[ $(command -v pip) ]]
+then
+    # Install pywal if it's not already (needed for colorscheme)
+    if [[ ! $(pip list | grep -E "^pywal") ]]
+    then
+        pip install --user pywal
+    fi
+fi
+
+# Install vim-plug and install or update plugins
+if [[ ! -f 'autoload/plug.vim' ]]
+then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     vim +PlugInstall +qall
 else
-    # Update Vim configuration
-    git pull
-    # Update vim-plug and plugins and remove unused ones
     vim +PlugUpgrade +PlugClean! +PlugUpdate +qall
 fi
