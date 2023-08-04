7e805e3cbe9f4519c7c49cc68516787081216693
HanlonsStraightRazor <muellermartin98@gmail.com>
2023-08-04 17:42:40 -0500

    Add helpful messages when installing pywal.
    Gentoo started getting sad when trying to install Python packages (even
    just for one user). I've added some warnings for when pip isn't
    installed or when pip fails to install pywal.

--- .vim/update.sh
+++ .vim/update.sh
@@ -8,10 +8,24 @@
 if [[ $(command -v pip) ]]
 then
     # Install pywal if it's not already (needed for colorscheme)
-    if [[ ! $(pip list | grep -E "^pywal") ]]
+    if [[ ! $(pip list | grep -E "^pywal\b") ]]
     then
-        pip install --user pywal
+        echo "Warning: pywal is not installed." >&2
+        echo -n "Installing pywal..."
+        message=$(pip install --user pywal 2>&1)
+        if [[ $? ]]
+        then
+            echo "Done"
+        else
+            echo "Failed" >&2
+            echo "Please install pywal manually." >&2
+            echo "Error message:" >&2
+            echo "$message" >&2
+        fi
     fi
+else
+    echo "Warning: pip is not installed." >&2
+    echo "Please install pip to use the correct colorscheme." >&2
 fi
 
 # Install vim-plug and install or update plugins
